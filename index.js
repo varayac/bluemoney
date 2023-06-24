0616a6cdef899a50e9a084b815c183404a8748f0
Víctor Araya <victor.d.araya@gmail.com>
2023-06-24 13:29:16 -0400

    crear child process

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const child_process = require('child_process')
+// const { spawn } = require('child_process')
+
+// const child = spawn('pwd')
+
+// child.stdout
+//   .on('data', (d) => {
+//     console.log('child stdout: ', d.toString())
+//   })
+//   .on('message', (men) => {
+//     console.log('algo:', men)
+//   })
+
+// child.on('exit', (code, signal) => {
+//   console.log('child process exited with ' + `code ${code} and signal ${signal}`)
+// })
