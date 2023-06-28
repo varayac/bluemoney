aa34412fc086892208688aa4e987a94c141ea879
Víctor Araya <victor.d.araya@gmail.com>
2023-06-27 23:01:33 -0400

    Se agregan semicolons (;) y listener end en la llamada a la API

--- app.js
+++ app.js
@@ -1,81 +1,79 @@
-const https = require('https')
-const fs = require('fs')
+const https = require('https');
+const fs = require('fs');
 
 // Recibir datos por línea de comando
-const [, , nombreArchivo, tipoDivisa, cantidadPesos] = process.argv
-const pesos = Number(cantidadPesos)
+const [, , nombreArchivo, tipoDivisa, cantidadPesos] = process.argv;
+const pesos = Number(cantidadPesos);
 
 // Consultar API
 const obtenerDatos = () => {
-  const URL_BASE = 'https://mindicador.cl/api'
-  return new Promise((resolve, reject) => {
-    https.get(URL_BASE, (res) => {
-      res
-        .on('data', (data) => {
-          const dataObtenida = JSON.parse(data)
-          resolve(dataObtenida)
-        })
-        .on('error', (err) => {
-          reject(err.message)
-        })
-    })
-  })
-}
+	const URL_BASE = 'https://mindicador.cl/api';
+	return new Promise((resolve, reject) => {
+		let data = '';
+		https.get(URL_BASE, (res) => {
+			res.on('data', (chunk) => {
+				data += chunk;
+			});
+			res.on('end', () => {
+				const dataObtenida = JSON.parse(data);
+				resolve(dataObtenida);
+			}).on('error', (err) => {
+				reject(err.message);
+			});
+		});
+	});
+};
 
 // Transformar divisa
 const calcularDivisa = (dataObtenida) => {
-  return new Promise((resolve, reject) => {
-    if (!Object.keys(dataObtenida).includes(tipoDivisa)) {
-      reject(new Error('Divisa no encontrada'))
-    }
+	return new Promise((resolve, reject) => {
+		if (!Object.keys(dataObtenida).includes(tipoDivisa)) {
+			reject(new Error('Divisa no encontrada'));
+		}
 
-    // Monto en CLP / Tasa de cambio = Monto en en otra moneda.
-    const valorConversion = pesos / dataObtenida[tipoDivisa].valor
-    resolve(valorConversion)
-  })
-}
+		// Monto en CLP / Tasa de cambio = Monto en en otra moneda.
+		const valorConversion = pesos / dataObtenida[tipoDivisa].valor;
+		resolve(valorConversion);
+	});
+};
 
 // Crear cotización
 const crearCotizacion = (totalConversion) => {
-  fs.writeFile(
-    `${nombreArchivo}`,
-    `A la fecha: ${Date()} \n` +
-      `Fue realizada cotización con los siguientes datos: \n` +
-      `Cantidad de pesos a convertir: $${pesos.toLocaleString('ES-cl')} pesos \n` +
-      `Convertido a '${tipoDivisa}' da un total de: ${totalConversion.toFixed(2)}`,
-    'utf8',
-    () => {
-      console.log(`Archivo ${nombreArchivo} creado con éxito`)
-    }
-  )
-}
+	fs.writeFile(
+		`${nombreArchivo}`,
+		`A la fecha: ${Date()} \n` +
+			`Fue realizada cotización con los siguientes datos: \n` +
+			`Cantidad de pesos a convertir: $${pesos.toLocaleString('ES-cl')} pesos \n` +
+			`Convertido a '${tipoDivisa}' da un total de: ${totalConversion.toFixed(2)}`,
+		'utf8',
+		() => {
+			console.log(`Archivo ${nombreArchivo} creado con éxito`);
+		}
+	);
+};
 
-const crearRespaldoHistorico = () => {
-  fs.writeFile(``)
-}
-
-// LEER ARCHIVO
+// Leer Archivo
 const leerArchivo = () => {
-  return new Promise((resolv, reject) => {
-    fs.readFile(nombreArchivo, 'utf8', (err, data) => {
-      if (err) {
-        reject('Error al leer archivo')
-      }
-      resolv(data)
-    })
-  })
-}
+	return new Promise((resolve, reject) => {
+		fs.readFile(nombreArchivo, 'utf8', (err, data) => {
+			if (err) {
+				reject('Error al leer archivo');
+			}
+			resolve(data);
+		});
+	});
+};
 
 const main = async () => {
-  try {
-    const dataObtenida = await obtenerDatos()
-    const totalConversion = await calcularDivisa(dataObtenida)
-    crearCotizacion(totalConversion)
-    const respuesta = await leerArchivo()
-    console.log(respuesta)
-  } catch (error) {
-    console.log(error.message)
-  }
-}
+	try {
+		const dataObtenida = await obtenerDatos();
+		const totalConversion = await calcularDivisa(dataObtenida);
+		crearCotizacion(totalConversion);
+		const respuesta = await leerArchivo();
+		console.log(respuesta);
+	} catch (error) {
+		console.log(error.message);
+	}
+};
 
-main()
+main();
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const child_process = require('child_process')
-const [, , nombreArchivo, extensionArchivo, divisa, cantidad] = process.argv
+const child_process = require('child_process');
+const [, , nombreArchivo, extensionArchivo, divisa, cantidad] = process.argv;
 
 // Concatenar nombre de archivo y extensión
-const nombreArchivoExtension = (archivo, extension) => `${archivo}.${extension}`
+const nombreArchivoExtension = (archivo, extension) => `${archivo}.${extension}`;
 
-const ejecutar =(archivo, nombreArchivo, divisa, cantidad) =>{
-  return new Promise((resolve) => {
-    child_process.exec(
-      `node ${archivo} ${nombreArchivo} ${divisa} ${cantidad}`,
-      (err, result) => {
-        if (err) {
-          console.log('Error en Child: ', err.message)
-        }
-        resolve(result)
-      }
-    )
-  })
-}
+const ejecutar = (archivo, nombreArchivo, divisa, cantidad) => {
+	return new Promise((resolve, reject) => {
+		child_process.exec(
+			`node ${archivo} ${nombreArchivo} ${divisa} ${cantidad}`,
+			(err, result) => {
+				if (err) {
+					reject('Error en Child: ', err.message);
+				}
+				resolve(result);
+			}
+		);
+	});
+};
 
-;(async () => {
-  await ejecutar(
-    'app.js',
-    nombreArchivoExtension(nombreArchivo, extensionArchivo),
-    divisa,
-    cantidad
-  )
-})()
+(async () => {
+	await ejecutar(
+		'app.js',
+		nombreArchivoExtension(nombreArchivo, extensionArchivo),
+		divisa,
+		cantidad
+	);
+})();
